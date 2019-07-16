c41be106c781bb9063fa72db6604b772c8da6b53
Mandar Kulkarni <52269075+mandar6988@users.noreply.github.com>
2019-07-16 21:52:48 +0530

    Add files via upload

---
+++ data/process_data.py
@@ -0,0 +1,70 @@
+import sys
+import pandas as pd
+import numpy as np
+import random
+from sqlalchemy import create_engine
+import sqlite3
+
+def load_data(messages_filepath, categories_filepath):
+    messages = pd.read_csv(messages_filepath)
+    categories =pd.read_csv(categories_filepath)
+    
+    df =pd.merge(messages,categories)
+    
+    return df
+
+
+def clean_data(df):
+    categories = df.categories.str.split(pat=';',expand=True)
+    row = categories.loc[0]
+    category_colnames = row.apply(lambda temp1: temp1[0:-2])
+    categories.columns = category_colnames
+    for column in categories:
+    # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1]
+    
+    # convert column from string to numeric
+        categories[column] = categories[column].astype(int) 
+    df=df.drop(['categories','original'],axis=1)
+    
+    df = pd.concat([df,categories],axis=1)
+    temp=df.duplicated().sum()
+    print("Total Number of duplicates=",temp)
+    df=df.drop_duplicates()
+    print(type(df))
+    print(df.shape)
+    return df
+def save_data(df, database_filename):
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    df.to_sql('message_table',engine,index=False)
+    
+    pass
+    
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        
+        save_data(df, database_filepath)
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
